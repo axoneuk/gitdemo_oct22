4569acf31dda9cc68b748cc1d8a3e78ef3af1e7f
ramduvvuri <ram.duvvuri@gmail.com>
2022-10-02 08:21:42 +0100

    Jira 123 - Committing new project first time

---
+++ src/test/java/uk/axone/devintest/git/test/App_Tests.java
@@ -0,0 +1,12 @@
+package uk.axone.devintest.git.test;
+
+import org.junit.Test;
+
+public class App_Tests {
+
+    @Test
+    public void test1(){
+        System.out.println("running test 1");
+    }
+
+}
